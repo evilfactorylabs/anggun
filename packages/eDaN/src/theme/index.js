9b72b3ed7140a7843991eb1e1611f161d977f16b
ri7nz <ri7nz.labs@gmail.com>
2020-01-19 23:56:00 +0700

    feat: fonts & space add in theme

--- packages/eDaN/src/theme/index.js
+++ packages/eDaN/src/theme/index.js
@@ -1,4 +1,14 @@
 import colors from './colors'
-export {
-    colors
+import typography from './typography'
+
+const space = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+const breakpoints = [] //TODO  define type breakpoints size  (e.g: xl, xxl, xyz)
+
+const theme = {
+    space,
+    colors,
+    breakpoints,
+    ...typography,
 }
+
+export default theme
---
+++ packages/eDaN/src/theme/typography.js
@@ -0,0 +1,9 @@
+const fonts = {
+    heading: `Merriweather`,
+    body: `Inter`,
+    mono: `"Space Mono"`
+}
+
+export default {
+    fonts
+}
