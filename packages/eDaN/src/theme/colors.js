99ea81db5476ae0ece490f8cafbf02e71448952d
ri7nz <ri7nz.labs@gmail.com>
2020-01-19 21:56:25 +0700

    feat: theme colors

---
+++ packages/eDaN/src/theme/colors.js
@@ -0,0 +1,11 @@
+/*
+ * @see {https://github.com/evilfactorylabsarchive/design#colors}
+ */
+const colors = {
+    red: {
+        666: "#E53A40",
+        999: "#D01C22"
+    }
+}
+
+export default colors
