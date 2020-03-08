e4a9db490fd285f7ea2c4fa7bcc7a817bd22570e
Kevin Anantha <kevin.anantha@gmail.com>
2020-03-08 20:19:19 +0700

    ref(cra): remove unnecessary code ref(edan-core): add ThemeProvider

--- packages/edan-core/src/ThemeProvider/index.js
+++ packages/edan-core/src/ThemeProvider/index.js
@@ -2,10 +2,10 @@ import { createElement } from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import { default as defaultTheme } from '../theme'
 
-const Theme = ({children, ...props}) => createElement(ThemeProvider, props, children)
+const Theme = ({ children, ...props }) => createElement(ThemeProvider, props, children)
 
 Theme.defaultProps = {
-    theme: defaultTheme
+  theme: defaultTheme
 }
 
-export default Theme;
+export default Theme
