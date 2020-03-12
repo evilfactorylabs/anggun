9c43e8f1e7b631dd6fa51a7db947380843746a24
Kevin Anantha <kevin.anantha@gmail.com>
2020-03-13 01:58:12 +0700

    docs(storybook): add storybook

---
+++ examples/cra/src/stories/1-Button.stories.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { ThemeProvider, Box as BoxComponent } from '@evilfactory/edan-core'
+import { withKnobs, text, number } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
+
+export default {
+  title: 'Button',
+  component: BoxComponent,
+  decorators: [withKnobs, withA11y]
+}
+
+export const Text = () => <BoxComponent onClick={action('clicked')}>Hello Button</BoxComponent>
+
+export const Box = () => (
+  <ThemeProvider>
+    <BoxComponent
+      rounded={text('rounded', 'md')}
+      border={text('border', '1 px solid')}
+      as={text('as', 'div')}
+      color={text('color', 'white')}
+      bg={text('bg', '#FF5656')}
+      my={number('my', 2)}
+      py={number('py', 2)}
+      px={number('px', 2)}
+      p={number('p', 2)}
+    >
+      I am the box
+    </BoxComponent>
+  </ThemeProvider>
+)
