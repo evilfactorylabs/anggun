e4c2ee69603fed0b6ea85de66cd7f67be4e1581f
Kevin Anantha <kevin.anantha@gmail.com>
2020-03-14 15:33:54 +0700

    chore(gitignore): remove .storybook

---
+++ examples/cra/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ["../src/**/*.stories.js"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-a11y/register"
+  ]
+};
